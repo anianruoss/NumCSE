a04376b35b9d753c37c31df341e0c557f2641080
Anian Ruoss <anian.ruoss@gmail.com>
2017-10-09 01:02:01 +0200

    Add funcTimer and createRandNums

--- ex00/factorial/main.cpp
+++ ex00/factorial/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
 
 
 template<typename T>
 T recurFact(const T n) {
-    if (n <= 1) {
+    int n_int = n;
+    if (n_int <= 1) {
         return 1;
     } else {
-        return n * recurFact(n-1);
+        return n_int * recurFact(n_int-1);
     }
 }
 
@@ -21,14 +24,43 @@ T iterFact(const T n) {
     return fact;
 }
 
+template <typename T>
+double funcTimer(T (f)(T), int *input, int inputL) {
+    auto start = std::chrono::system_clock::now();
+
+    for (int i = 0; i < inputL; ++i) {
+        f(input[i]);
+    }
+
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = end - start;
+
+    return elapsed.count();
+}
+
+int* createRandNums(int N, int k1, int k2) {
+    int *randNums = new int[N];
+    srand(time(NULL));
+
+    for (int i = 0; i < N; ++i) {
+        randNums[i] = k1 + (std::rand() % (k2 - k1 + 1));
+    }
+
+    return randNums;
+}
+
 
 int main() {
-    std::cout << "Enter number: ";
-    int n;
-    std::cin >> n;
+    int N = std::pow(10,7), k1 = 5, k2 = 25;
 
-    std::cout << "Recursive factorial: " << recurFact(n) << std::endl;
-    std::cout << "Iterative factorial: " << iterFact(n) << std::endl;
+    int *randNums = createRandNums(N,k1,k2);
+
+    double recFactTime =funcTimer(recurFact<long>, randNums, N);
+    double iterFactTime = funcTimer(iterFact<long>, randNums, N);
+
+    std::cout << "Recursive: " << recFactTime	<< std::endl;
+    std::cout << "Iterative: " <<  iterFactTime << std::endl;
+    std::cout << "Ratio: " << recFactTime / iterFactTime << std::endl;
 
     return 0;
 }
