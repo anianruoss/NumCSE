90204e69431d5eb9962b5ae9e51920f0536ed61d
Anian Ruoss <anian.ruoss@gmail.com>
2017-11-01 02:19:26 +0100

    Initial commit problem4.1

---
+++ ex04/frobenius-least-squares/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/KroneckerProduct>
+
+
+using namespace Eigen;
+
+MatrixXd min_frob(const VectorXd &z, const VectorXd &g) {
+    const int n = z.size();
+
+    MatrixXd C = kroneckerProduct(MatrixXd::Identity(n,n), z.transpose());
+
+    MatrixXd CCT = -C*C.transpose();
+    VectorXd p = CCT.fullPivLu().solve(g);
+
+    VectorXd lamdba = -C.transpose()*p;
+
+    MatrixXd M(n,n);
+
+    for (int i = 0; i < n; ++i) {
+        M.row(i) = lamdba.segment(n*i,n);
+    }
+
+    return M;
+}
+
+
+int main() {
+    int n = 10;
+
+    VectorXd z = VectorXd::Random(n);
+    VectorXd g = VectorXd::Random(n);
+
+    MatrixXd M_augmNormal = min_frob(z, g);;
+    MatrixXd M_lagrange = g*z.transpose() / z.squaredNorm();
+
+    std::cout << "Error = " << (M_augmNormal - M_lagrange).norm() << std::endl;
+
+    return 0;
+}
+
