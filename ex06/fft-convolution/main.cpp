a1a7f52f94e4a69c1bb1ba4e27f93f8085d835d6
Anian Ruoss <anian.ruoss@gmail.com>
2017-11-12 22:27:49 +0100

    Implement discrete convolution

--- ex06/fft-convolution/main.cpp
+++ ex06/fft-convolution/main.cpp
@@ -1,64 +1,84 @@
-// run with: g++ -std=gnu++11 -I /usr/include/eigen3/ -g conv.cpp -lmgl 
 #include <iostream>
-#include <Eigen/Dense>
+#include <eigen3/Eigen/Dense>
 #include <cmath>
 #include <unsupported/Eigen/FFT>
 #include <mgl2/mgl.h>
 
+#include <typeinfo>
+
 using namespace Eigen;
 
-VectorXd PointA(const VectorXd &x, const VectorXd &y) {
-	VectorXd out;
-	// TODO
-	return out;
+
+
+VectorXd discreteConvolution(const VectorXd &x, const VectorXd &y) {
+    using index_t = MatrixXd::Index;
+    index_t m = x.size(), n = y.size();
+
+    VectorXd out(m+n-1);
+
+    for (index_t k = 0; k < m+n-1; ++k) {
+        for (index_t j = 0; j < m; ++j) {
+            if ((k-j >= 0) && (k-j < n)) {
+                out(k) += x(j) * y(k-j);
+            }
+        }
+    }
+
+    return out;
 }
 
 VectorXd PointB(const VectorXd &x, const VectorXd &y) {
-	VectorXd out;
-	// TODO
-	return out;
+    VectorXd out;
+    // TODO
+
+    return out;
 }
 
 VectorXd PointC(const VectorXd &x, const VectorXd &y) {
-	VectorXd out;
-	// TODO
-	return out;
+    VectorXd out;
+    // TODO
+
+    return out;
 }
+
 
 int main() {
-	srand(time(NULL));
-	VectorXd input = VectorXd::Zero(100);
-	VectorXd noise = VectorXd::Random(100); 
-	input += (noise.array() - 0.5).matrix() / 5;
+    srand(time(NULL));
+    VectorXd input = VectorXd::Zero(100);
+    VectorXd noise = VectorXd::Random(100);
+    input += (noise.array() - 0.5).matrix() / 5;
 
-	// define truncated gaussian filter
-	int N = 5;
-	double a = .5;
-	VectorXd gaussFilter(2*N+1);
-	for (int j=0; j < 2*N+1; j++) {
-		gaussFilter(j) = std::sqrt(a/3.1416) * std::exp(-a * ((j-N)) * (j-N));
-	}
+    // define truncated gaussian filter
+    int N = 5;
+    double a = .5;
+    VectorXd gaussFilter(2*N+1);
+    for (int j=0; j < 2*N+1; j++) {
+        gaussFilter(j) = std::sqrt(a/3.1416) * std::exp(-a * ((j-N)) * (j-N));
+    }
 
-	// Plotting
-	mglGraph gr, grA, grB, grC;
-	mglData dat, datA, datB, datC;
-	dat.Link(input.data(), input.size());
-	gr.Plot(dat);
-	gr.WriteFrame("noisySignal.png");
-	auto convA = PointA(input, gaussFilter);
-  	datA.Link(convA.data(), convA.size());
-	gr.Plot(datA);
-	grA.Plot(datA);
-	grA.WriteFrame("filteredA.png");
-	auto convB = PointB(input, gaussFilter);
-  	datB.Link(convB.data(), convB.size());
-	gr.Plot(datB);
-	grB.Plot(datB);
-	grB.WriteFrame("filteredB.png");
-	auto convC = PointC(input, gaussFilter);
-  	datC.Link(convC.data(), convC.size());
-	gr.Plot(datC);
-	grC.Plot(datC);
-	grC.WriteFrame("filteredC.png");
-	gr.WriteFrame("noisy+filters.png");
+    // Plotting
+    mglGraph gr, grA, grB, grC;
+    mglData dat, datA, datB, datC;
+    dat.Link(input.data(), input.size());
+    gr.Plot(dat);
+    gr.WriteFrame("bin/data/noisySignal.png");
+    auto convA = discreteConvolution(input, gaussFilter);
+    datA.Link(convA.data(), convA.size());
+    gr.Plot(datA);
+    grA.Plot(datA);
+    grA.WriteFrame("bin/data/filteredA.png");
+    auto convB = PointB(input, gaussFilter);
+    datB.Link(convB.data(), convB.size());
+    gr.Plot(datB);
+    grB.Plot(datB);
+    grB.WriteFrame("bin/data/filteredB.png");
+    auto convC = PointC(input, gaussFilter);
+    datC.Link(convC.data(), convC.size());
+    gr.Plot(datC);
+    grC.Plot(datC);
+    grC.WriteFrame("bin/data/filteredC.png");
+    gr.WriteFrame("bin/data/noisy+filters.png");
+
+    return 0;
 }
+
