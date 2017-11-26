dacf7edfcc897866b353a4e809f22f971bc184f4
Anian Ruoss <anian.ruoss@gmail.com>
2017-11-26 17:51:35 +0100

    Implement Aitken-Neville algorithm

--- ex07/aitken-neville/main.cpp
+++ ex07/aitken-neville/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <eigen3/Eigen/Dense>
+#include <iostream>
 
 
 using namespace Eigen;
@@ -14,10 +14,20 @@ using namespace Eigen;
 double derivIpolEvalAN(const VectorXd & t,
                        const VectorXd & y,
                        const double x) {
-
     assert(t.size() == y.size());
-    // TODO
 
+    VectorXd p(y);
+    VectorXd dP = VectorXd::Zero(y.size());
+
+    for (int i = 0; i < y.size(); ++i) {
+        for (int k = i-1; k >= 0; --k) {
+            double s = (x-t(k))*dP(k+1) - (x-t(i))*dP(k) + p(k+1) - p(k);
+			dP(k) = s / (t(i) - t(k)); 
+            p(k) = p(k+1) + (p(k+1) - p(k))*(x - t(i))/(t(i) - t(k));
+        }
+    }
+
+    return dP(0);
 }
 
 
@@ -28,7 +38,8 @@ int main() {
     y << 2, -4, 6;
 
     double x = 0.5;
-    std::cout<< "Polynomial derivative @ x = " << x << " is: "<< derivIpolEvalAN(t,y,x) << std::endl;
+    std::cout<< "Polynomial derivative @ x = " << x
+             << " is: "<< derivIpolEvalAN(t,y,x) << std::endl;
 
     return 0;
 }
