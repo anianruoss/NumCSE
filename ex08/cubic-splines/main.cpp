8bbc000c55927da74872521a2d057db2f933c786
Anian Ruoss <anian.ruoss@gmail.com>
2017-11-26 23:04:03 +0100

    Evaluate splines and plot results

--- ex08/cubic-splines/main.cpp
+++ ex08/cubic-splines/main.cpp
@@ -13,37 +13,37 @@ MatrixXd cubicSpline(const VectorXd &T, const VectorXd &Y) {
     int n = T.size() - 1; // T and Y have length n+1
 
     MatrixXd spline(4, n);
-	spline.row(0) = Y.head(n).transpose();
+    spline.row(0) = Y.head(n).transpose();
 
-	VectorXd h(n);
+    VectorXd h(n);
 
-	for (int i = 0; i < n; ++i) {
-		h(i) = T(i+1) - T(i);
-	}
+    for (int i = 0; i < n; ++i) {
+        h(i) = T(i+1) - T(i);
+    }
 
-	MatrixXd triDiag = MatrixXd::Zero(n-1,n-1);
-	triDiag(0,0) = (h(0) + h(1))/3;
+    MatrixXd triDiag = MatrixXd::Zero(n-1,n-1);
+    triDiag(0,0) = (h(0) + h(1))/3;
 
-	for (int i = 1; i < n-1; ++i) {
-		triDiag(i,i) = (h(i) + h(i+1))/3;
-		triDiag(i,i-1) = h(i)/6;
-		triDiag(i-1,i) = h(i)/6;
-	}
-	
-	VectorXd r(n-1);
+    for (int i = 1; i < n-1; ++i) {
+        triDiag(i,i) = (h(i) + h(i+1))/3;
+        triDiag(i,i-1) = h(i)/6;
+        triDiag(i-1,i) = h(i)/6;
+    }
 
-	for (int i = 0; i < n-1; ++i) {
-		r(i) = (Y(i+2) - Y(i+1))/h(i+1) - (Y(i+1) - Y(i))/h(i);
-	}
+    VectorXd r(n-1);
 
-	VectorXd mu = VectorXd::Zero(n+1);
-	mu.head(n).tail(n-1) = triDiag.fullPivLu().solve(r);
+    for (int i = 0; i < n-1; ++i) {
+        r(i) = (Y(i+2) - Y(i+1))/h(i+1) - (Y(i+1) - Y(i))/h(i);
+    }
 
-	for (int i = 0; i < n; ++i) {
-		spline(1,i) = (Y(i+1) - Y(i))/h(i) - h(i)*(2*mu(i) + mu(i))/6;
-		spline(2,i) = mu(i)/2;
-		spline(3,i) = (mu(i+1) - mu(i))/(6*h(i));
-	}
+    VectorXd mu = VectorXd::Zero(n+1);
+    mu.head(n).tail(n-1) = triDiag.fullPivLu().solve(r);
+
+    for (int i = 0; i < n; ++i) {
+        spline(1,i) = (Y(i+1) - Y(i))/h(i) - h(i)*(2*mu(i) + mu(i))/6;
+        spline(2,i) = mu(i)/2;
+        spline(3,i) = (mu(i+1) - mu(i))/(6*h(i));
+    }
 
     return spline;
 }
@@ -52,10 +52,20 @@ MatrixXd cubicSpline(const VectorXd &T, const VectorXd &Y) {
 // Assumes T is sorted, with no repetetions.
 VectorXd evalCubicSpline(const MatrixXd &S, const VectorXd &T,
                          const VectorXd &evalT) {
-    int n = evalT.size();
-    VectorXd out(n);
+	const int s = T.size();
+    const int n = evalT.size();
+    VectorXd out = VectorXd::Zero(n);
 
-    // TODO: fill out
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < s-1; ++i) {
+            if (T(i+1) >= evalT(k)) {
+                out(k) += S(0,i) + S(1,i)*evalT(k);
+                out(k) += S(2,i) * std::pow(evalT(k),2);
+                out(k) += S(3,i) * std::pow(evalT(k),3);
+                break;
+            }
+        }
+    }
 
     return out;
 }
@@ -72,14 +82,29 @@ int main() {
 
     VectorXd evalSpline = evalCubicSpline(cubicSpline(T, Y), T, evalT);
 
+	std::vector<double> tvec(T.data(), T.data() + T.rows()*T.cols());
+	std::vector<double> yvec(Y.data(), Y.data() + Y.rows()*Y.cols());
+
+	double *t = &tvec[0];
+	double *y = &yvec[0];
+
+	mglData refx, refy;
+	refx.Link(t, 9);
+	refy.Link(y, 9);
+
     mglData datx, daty;
     datx.Link(evalT.data(), len);
     daty.Link(evalSpline.data(), len);
+	
     mglGraph gr;
     gr.SetRanges(0, 2, -3, 3);
-    gr.Plot(datx, daty, "0");
+    gr.Plot(datx, daty, "b");
+	gr.AddLegend("Splines", "b");
+	gr.Plot(refx, refy, "g *");
+	gr.AddLegend("Reference Points", "g *");
+	gr.Legend(2);
     gr.WriteFrame("spline.eps");
-
+	
     return 0;
 }
 
