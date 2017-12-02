a30f4d3b114407ab19f56baae7d3f5e34b01be9c
Anian Ruoss <anian.ruoss@gmail.com>
2017-12-02 23:11:31 +0100

    Implement divided differences scheme

--- ex09/adaptive-interpolation/newtonIpol.hpp
+++ ex09/adaptive-interpolation/newtonIpol.hpp
@@ -6,10 +6,17 @@ using namespace Eigen;
 // IN: t = node set (mutually different)
 // y = nodal values
 // OUT: coeffs = coefficients of polynomial in Newton basis
-VectorXd divDiff(const VectorXd& t, const VectorXd& y) {
+VectorXd divDiff(const VectorXd &t, const VectorXd &y) {
+    const unsigned int n = y.size() - 1;
+    VectorXd coeffs(y);
 
-    // TODO
+    for (unsigned int i = 0; i < n; ++i) {
+        for (unsigned int j = i; j < n; ++j) {
+            coeffs(j+1) = (coeffs(j+1)-coeffs(i)) / (t(j+1)-t(i));
+        }
+    }
 
+    return coeffs;
 }
 
 // Evaluation of polynomial in Newton basis (divided differences)
@@ -17,18 +24,19 @@ VectorXd divDiff(const VectorXd& t, const VectorXd& y) {
 // y = values in t
 // x = evaluation points (as Eigen::Vector)
 // OUT: p = values in x */
-void intPolyEval (const VectorXd& t, const VectorXd& y, const VectorXd& x, VectorXd& p) {
+void intPolyEval (const VectorXd &t, const VectorXd &y, const VectorXd &x,
+                  VectorXd &p) {
+    const unsigned n = y.size() - 1;
 
-    const unsigned n = y.size()-1 ;
     // get Newton coefficients of polynomial (non in-situ implementation!)
-    VectorXd coeffs;
-    coeffs = divDiff(t, y);
+    VectorXd coeffs = divDiff(t, y);
 
     // evaluate
     VectorXd ones = VectorXd::Ones(x.size());
     p = coeffs(n)*ones;
 
-    for (int j=n-1; j>= 0; --j)
+    for (int j = n-1; j >= 0; --j) {
         p = (x - t(j)*ones).cwiseProduct(p) + coeffs(j)*ones;
+    }
 }
 
