5ceed9f4daa61d4867ceb6d13bc50174bd3d9fdc
Anian Ruoss <anian.ruoss@gmail.com>
2017-12-09 16:19:23 +0100

    Plot error vs step number

--- ex09/adaptive-interpolation/main.cpp
+++ ex09/adaptive-interpolation/main.cpp
@@ -1,6 +1,7 @@
 #include "newtonIpol.hpp"
 #include <mgl2/mgl.h>
 #include <iostream>
+#include <string>
 
 
 using namespace Eigen;
@@ -8,29 +9,41 @@ using namespace Eigen;
 template <class Function>
 void adapPolyIpol(const Function &f, const double a, const double b,
                   const double tol, const unsigned N,
-                  VectorXd &adaptive_nodes, VectorXd &error_vs_step_no) {
+                  VectorXd &adaptive_nodes, VectorXd &error_vs_step_no,
+                  std::string frameName) {
     // Generate sampling points and evaluate $f$ there
     VectorXd sampling_points = VectorXd::LinSpaced(N, a, b);
     VectorXd fvals_at_sampling_points = sampling_points.unaryExpr(f);
 
     adaptive_nodes(0) = 0.5*(b+a);
-    VectorXd y = adaptive_nodes.unaryExpr(f);
-    VectorXd poly;
-    intPolyEval(adaptive_nodes, y, sampling_points, poly);
+    VectorXd y, poly;
 
-    while ((fvals_at_sampling_points - poly).maxCoeff()
-            > tol * fvals_at_sampling_points.maxCoeff()) {
+    double fMax = fvals_at_sampling_points.cwiseAbs().maxCoeff();
+
+    for (unsigned int i = 0; i < N; ++i) {
         y = adaptive_nodes.unaryExpr(f);
         intPolyEval(adaptive_nodes, y, sampling_points, poly);
 
-        // calculate new node
-        VectorXd::Index maxDiffIdx;
-        double error = (fvals_at_sampling_points - poly).maxCoeff(&maxDiffIdx);
+        // calculate maximum error
+        VectorXd::Index maxErrorIdx;
+        VectorXd errors = (fvals_at_sampling_points - poly).cwiseAbs();
+        double maxError = errors.maxCoeff(&maxErrorIdx);
+
+        // check termination criterium
+        if (maxError <= tol * fMax) {
+            break;
+        }
+
+        // add node to set and save the error
         adaptive_nodes.conservativeResize(adaptive_nodes.size()+1);
-        adaptive_nodes(adaptive_nodes.size()-1) = sampling_points(maxDiffIdx);
+        adaptive_nodes(adaptive_nodes.size()-1) = sampling_points(maxErrorIdx);
 
         error_vs_step_no.conservativeResize(error_vs_step_no.size()+1);
-        error_vs_step_no(error_vs_step_no.size()-1) = std::abs(error);
+        error_vs_step_no(error_vs_step_no.size()-1) = maxError;
+
+        if (i == N-1) {
+            std::cerr << "Desired accuracy could not be reached." << std::endl;
+        }
     }
 
     // sort nodes for plotting
@@ -59,7 +72,8 @@ void adapPolyIpol(const Function &f, const double a, const double b,
     gr.AddLegend("Interpolation", "b");
     gr.Legend(0);
     gr.Title("Adaptive Polynomial Interpolation");
-    gr.WriteFrame("plots/adapPolyIpol.eps");
+    std::string name = "plots/adapPolyIpol-" + frameName + ".eps";
+    gr.WriteFrame(name.c_str());
 }
 
 
@@ -77,21 +91,44 @@ int main() {
     double tol = 1e-6;
     unsigned int N = 1000;
 
-    VectorXd adaptive_nodes = VectorXd::Zero(1);
-    VectorXd error_vs_step_no(adaptive_nodes);
+    VectorXd adaptive_nodes_f1 = VectorXd::Zero(1);
+    VectorXd adaptive_nodes_f2 = VectorXd::Zero(1);
+    VectorXd error_vs_step_no_f1, error_vs_step_no_f2;
 
-    adapPolyIpol(f1, a, b, tol, N, adaptive_nodes, error_vs_step_no);
+    adapPolyIpol(f1, a, b, tol, N, adaptive_nodes_f1, error_vs_step_no_f1, "f1");
+    adapPolyIpol(f2, a, b, tol, N, adaptive_nodes_f2, error_vs_step_no_f2, "f2");
 
-    mglData daty;
-    daty.Link(error_vs_step_no.data(), error_vs_step_no.size());
+    VectorXd ref1 = VectorXd::LinSpaced(error_vs_step_no_f1.size(), 1,
+                                        error_vs_step_no_f1.size());
+    VectorXd ref2 = VectorXd::LinSpaced(error_vs_step_no_f2.size(), 1,
+                                        error_vs_step_no_f2.size());
+
+    mglData dat1x, dat2x;
+    dat1x.Link(ref1.data(), ref1.size());
+    dat2x.Link(ref2.data(), ref2.size());
+
+    mglData dat1y, dat2y;
+    dat1y.Link(error_vs_step_no_f1.data(), error_vs_step_no_f1.size());
+    dat2y.Link(error_vs_step_no_f2.data(), error_vs_step_no_f2.size());
+
+    double xMax = std::max(ref1.size(), ref2.size());
+    double yMax	= std::max(error_vs_step_no_f1.maxCoeff(),
+                           error_vs_step_no_f2.maxCoeff());
+    double yMin = std::min(error_vs_step_no_f1.minCoeff(),
+                           error_vs_step_no_f2.minCoeff());
 
     mglGraph gr;
-    gr.SetRanges(0, error_vs_step_no.size(), 0, error_vs_step_no.maxCoeff());
+    gr.Title("Maximum Interpolation Error");
+    gr.SetRanges(1, xMax, yMin, yMax);
+    gr.SetFunc("x","lg(y)");
     gr.Axis();
-    gr.Plot(daty, "b");
+    gr.Plot(dat1x, dat1y, "b+");
+    gr.AddLegend("f_1(t) = sin(e^{2t})","b+");
+    gr.Plot(dat2x, dat2y, "rs");
+    gr.AddLegend("f_2(t) = \\sqrt{t}/(1 + 16t^2)","rs");
     gr.Label('x',"Step [N]", 0);
     gr.Label('y', "Max. Error", 0);
-    gr.Title("Maximum Interpolation Error");
+    gr.Legend();
     gr.WriteFrame("plots/adapPolyIpol-maxError.eps");
 
     return 0;
