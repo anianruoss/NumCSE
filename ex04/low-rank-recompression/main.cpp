128c053a6543c6e0c24a58347ffc3ac28e03bc11
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-07 15:57:16 +0100

    Use safeMax in low-rank approximation

--- ex04/low-rank-recompression/main.cpp
+++ ex04/low-rank-recompression/main.cpp
@@ -1,20 +1,25 @@
-#include <iostream>
-#include <limits>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/QR>
 #include <eigen3/Eigen/SVD>
 
+#include <algorithm>
+#include <iostream>
+#include <limits>
+
 
 using namespace Eigen;
 
-void factorize(const MatrixXd & X, MatrixXd & A, MatrixXd & B) {
+void factorize(const MatrixXd & X, const int k, MatrixXd & A, MatrixXd & B) {
     JacobiSVD<MatrixXd> svd(X, ComputeThinU | ComputeThinV);
     MatrixXd U = svd.matrixU();
+    VectorXd s = svd.singularValues();
     MatrixXd V = svd.matrixV();
-    VectorXd sv = svd.singularValues();
 
-    A = U.array().rowwise() * sv.transpose().array();
-    B = V;
+    const int safeMax = std::min(k, static_cast<int>(s.size()));
+
+    A = U.leftCols(safeMax).array().rowwise() *
+        s.head(safeMax).transpose().array();
+    B = V.leftCols(safeMax);
 }
 
 // Complexity: O(n)	for k << n and m = n
@@ -43,15 +48,19 @@ void svd_AB(const MatrixXd & A, const MatrixXd & B,
 void rank_k_approx(const MatrixXd &Ax, const MatrixXd &Ay,
                    const MatrixXd &Bx, const MatrixXd &By,
                    MatrixXd &Az, MatrixXd &Bz) {
-    MatrixXd A(Ax.rows(), 2*Ax.cols());
+	assert(Ax.rows() == Ay.rows() && Ax.cols() == Ay.cols());
+	assert(Bx.rows() == By.rows() && Bx.cols() == By.cols());
+
+	using index_t = MatrixXd::Index;
+	const index_t m = Ax.rows(), k = Ax.cols(), n = Bx.cols();
+
+    MatrixXd A(m, 2*k), B(n, 2*k);
     A << Ax, Ay;
-    MatrixXd B(Bx.rows(), 2*Bx.cols());
     B << Bx, By;
 
     MatrixXd U, S, V;
     svd_AB(A, B, U, S, V);
 
-    int k = Ax.cols();
     Az = U.leftCols(k) * S;
     Bz = V.leftCols(k);
 }
@@ -67,12 +76,12 @@ int main() {
     X << 5, 0, 2, 1, 7, 4;
 
     MatrixXd A, B;
-    factorize(X, A, B);
+    factorize(X, k, A, B);
 
-    std::cout << "A =" << std::endl << A << std::endl << std::endl;
-    std::cout << "B =" << std::endl << B << std::endl << std::endl;
-    std::cout << "Error: " << (X - A*B.transpose()).norm() << std::endl;
-    std::cout << std::endl;
+    std::cout << "A =" << std::endl << A << std::endl << std::endl
+              << "B =" << std::endl << B << std::endl << std::endl
+              << "Error: " << (X - A*B.transpose()).norm() << std::endl
+			  << std::endl;
 
     std::cout << "--> Low Rank SVD" << std::endl;
     A.resize(m,k);
@@ -82,11 +91,11 @@ int main() {
     MatrixXd U, S, V;
 
     svd_AB(A, B, U, S, V);
-    std::cout << "U =" << std::endl << U << std::endl << std::endl;
-    std::cout << "S =" << std::endl << S << std::endl << std::endl;
-    std::cout << "V =" << std::endl << V << std::endl << std::endl;
-    std::cout << "Error: " << (A*B.transpose() - U*S*V.transpose()).norm() << std::endl;
-    std::cout << std::endl;
+    std::cout << "U =" << std::endl << U << std::endl << std::endl
+              << "S =" << std::endl << S << std::endl << std::endl
+              << "V =" << std::endl << V << std::endl << std::endl
+              << "Error: " << (A*B.transpose() - U*S*V.transpose()).norm()
+              << std::endl << std::endl;
 
     std::cout << "--> Low Rank Approximation" << std::endl;
     MatrixXd Ax(m,k), Ay(m,k), Bx(n,k), By(n,k), Az, Bz;
@@ -97,10 +106,10 @@ int main() {
 
     rank_k_approx(Ax, Ay, Bx, By, Az, Bz);
 
-    std::cout << "Az =" << std::endl << Az << std::endl << std::endl;
-    std::cout << "Bz =" << std::endl << Bz << std::endl << std::endl;
-    std::cout << "Error: "
-              << (Ax*Bx.transpose() + Ay*By.transpose() - Az*Bz.transpose()).norm()
+    std::cout << "Az =" << std::endl << Az << std::endl << std::endl
+              << "Bz =" << std::endl << Bz << std::endl << std::endl
+              << "Error: "
+              << (Ax*Bx.transpose()+Ay*By.transpose()-Az*Bz.transpose()).norm()
               << std::endl;
 
     return 0;
