cbb347d0af89e846eaf48e380d0822f3d6c67188
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-19 00:28:05 +0100

    Test Newton's method non-convergent initial guess

--- ex11/newtons-method/main.cpp
+++ ex11/newtons-method/main.cpp
@@ -7,12 +7,13 @@
 
 using namespace Eigen;
 
-/* @brief Newton's method to approximate $x^{(0)}$
+/* @brief Newton's method to approximate smallest positive intial guess
+ * $x^{(0)}$ for which Newton's method does not converge for $arctan(x)$
  * @param[in] x0_ Initial guess
  * @param[out] x0 Final estimation of $x^{(0)}$, given convergence of Newton's
  * method
  */
-double newton_arctan(double x0_) {
+double newtonInitialGuess(double x0_) {
     const double eps = std::numeric_limits<double>::epsilon();
     double update = 1.;
     double x0 = x0_, x1;
@@ -32,15 +33,51 @@ double newton_arctan(double x0_) {
 
         std::cout << "x0 = " << x0 << ", accuracy = " << update << std::endl;
     }
+    std::cout << std::endl;
 
     return x0;
 }
 
+/* @brief Newton's method to approximate root of $arctan(x)$
+ * @param[in] x0_ Initial guess
+ * @param[out] x0 Final estimation of root, given convergence of Newton's method
+ */
+double newtonArctan(double x0_) {
+    const size_t maxItr = 15;
+    const double eps = std::numeric_limits<double>::epsilon();
+    double update = 1.;
+    double x0 = x0_, x1;
+
+    for (size_t i = 0; i < maxItr; ++i) {
+        if (std::abs(update) <= eps) {
+            break;
+        } else {
+            x1 = x0 - std::atan(x0) * (1 + x0*x0);
+            update = (x1 - x0) / x1;
+            x0 = x1;
+
+            std::cout << "x0 = " << x0 << ", accuracy = " << update << std::endl;
+
+            if (i == maxItr - 1) {
+                std::cerr << std::endl
+                          << "Newton's method failed to converge after "
+                          << maxItr << " iterations." << std::endl
+						  << std::endl;
+            }
+        }
+    }
+
+    return x0;
+}
 
 int main() {
-    // Netwon's method
+    std::cout << "Calculating smallest positive non-convergent initial guess"
+              << std::endl;
     const double x0_ = 2; // Initial guess
-    double x0 = newton_arctan(x0_);
+    double x0 = newtonInitialGuess(x0_);
+
+    std::cout << "Testing initial guess on Newton's method" << std::endl;
+    double root = newtonArctan(x0);
 
     return 0;
 }
