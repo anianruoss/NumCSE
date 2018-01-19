f665b7984be7430169aefcabf7e3f57f3cf5e7ce
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-19 22:13:30 +0100

    Initialize matrix and compute rank-k approximation

--- exams/hs2016/problemX_rankk.cpp
+++ exams/hs2016/problemX_rankk.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 
 #include <Eigen/Dense>
+#include <Eigen/SVD>
 #include <unsupported/Eigen/FFT>
 
 using namespace Eigen;
@@ -11,17 +12,16 @@ using namespace Eigen;
  *! @param[in] h  Vector of entries of $\VC$.
  *! @return C The matrix $\mathbf{C}$ representing the linear mapping.
  */
-
 MatrixXd buildLTFIRMatrix(const VectorXd & h) {
-    // Initialization
-    unsigned int n = h.size();
-    MatrixXd C(2*n-1, n);
+    const size_t n = h.size();
+    MatrixXd C = MatrixXd::Zero(2*n-1, n);
 
-    // TODO: build and return the matrix $\VC$
+    for (size_t i = 0; i < n; ++i) {
+        C.col(i).tail(2*n-1-i).head(n) = h;
+    }
 
     return C;
 }
-
 
 /*!
  * \brief The LTFIR_lowrank class evaluates a LTFIR filter
@@ -33,30 +33,31 @@ class LTFIR_lowrank {
      *! @param[in]  h Vector of entries of matrix of the filter.
      *! @param[in]  k Rank of the approximation.
      */
-
     LTFIR_lowrank(const VectorXd& h, unsigned int k) {
         MatrixXd C = buildLTFIRMatrix(h);
 
-        // TODO: implement constructor of LTFIR\_lowrank
-    }
+        // complexity: O(n^3)
+        JacobiSVD<MatrixXd> svd(C, ComputeThinU | ComputeThinV);
+        MatrixXd Sigma = svd.singularValues().head(k).asDiagonal();
 
+        // complexity: O(k) (optimised product with diagonal matrix)
+        U_ = svd.matrixU().leftCols(k) * Sigma;
+        Vt_ = svd.matrixV().leftCols(k).transpose();
+    }
 
     /*! @brief Operator evaluating $\tilde{\mathbf{C}}\mathbf{x}$
      *! @param[in] x Input vector.
      *! @return      Output vector $y = \tilde{\mathbf{C}}\mathbf{x}$
      */
-
     VectorXd operator()(const VectorXd& x) const {
-        VectorXd y;
-
-        // TODO: implement evaluation operator $y = \tilde{\mathbf{C}}\mathbf{x}$.
-        return y;
+        // complexity: O(nk)
+        VectorXd tmp = Vt_ * x;
+        return U_ * tmp;
     }
 
   private:
-
-    // TODO: private members of LTFIR\_lowrank
-
+    MatrixXd U_;
+    MatrixXd Vt_;
 };
 
 /*!
@@ -70,7 +71,6 @@ class LTFIR_freq {
      *! @param[in]  h  Vector of entries for the matrix of the filter.
      *! @param[in]  k  Maximum number of frequencies (low-pass filter).
      */
-
     LTFIR_freq(const VectorXd& h, unsigned k) {
         n_ = h.size();
         k_ = k;
@@ -83,13 +83,11 @@ class LTFIR_freq {
         ch_ = fft.fwd(h_);
     }
 
-
     /*!
      *! @brief Low-pass filtering operator.
      *! @param[in]  x  Input vector (to be filtered).
      *! @return        Filtered output vector.
      */
-
     VectorXd operator()(const VectorXd& x) const {
         assert(x.size() == n_ && "x must have same length of h");
 
@@ -111,8 +109,8 @@ class LTFIR_freq {
     int n_;
     int k_;
     VectorXcd ch_;
-
 };
+
 
 int main() {
     unsigned int n = 6;
@@ -122,55 +120,51 @@ int main() {
     std::cout << "**** PART 1 ****" << std::endl;
 
     VectorXd h = VectorXd::LinSpaced(n,1,n);
-    std::cout << "h = "
-              << std::endl
-              << h
-              << std::endl;
+    std::cout << "h = " << h.transpose() << std::endl << std::endl;
 
-    std::cout << "Matrix C is:"
-              << std::endl
-              << buildLTFIRMatrix(h)
-              << std::endl;
+    MatrixXd C = buildLTFIRMatrix(h);
+    MatrixXd C_exact(2*n-1, n);
+    C_exact <<
+            1, 0, 0, 0, 0, 0,
+            2, 1, 0, 0, 0, 0,
+            3, 2, 1, 0, 0, 0,
+            4, 3, 2, 1, 0, 0,
+            5, 4, 3, 2, 1, 0,
+            6, 5, 4, 3, 2, 1,
+            0, 6, 5, 4, 3, 2,
+            0, 0, 6, 5, 4, 3,
+            0, 0, 0, 6, 5, 4,
+            0, 0, 0, 0, 6, 5,
+            0, 0, 0, 0, 0, 6;
 
-//    Matrix C should be:
-//    1 0 0 0 0 0
-//    2 1 0 0 0 0
-//    3 2 1 0 0 0
-//    4 3 2 1 0 0
-//    5 4 3 2 1 0
-//    6 5 4 3 2 1
-//    0 6 5 4 3 2
-//    0 0 6 5 4 3
-//    0 0 0 6 5 4
-//    0 0 0 0 6 5
-//    0 0 0 0 0 6
+    std::cout << "Matrix C is:" << std::endl
+              << C << std::endl << std::endl
+              << "Error: " << (C - C_exact).norm() << std::endl << std::endl;
 
     // PART 2: filter vector x (rank-k filter)
     std::cout << "**** PART 2 ****" << std::endl;
 
     VectorXd x = VectorXd::Ones(n);;
-    std::cout << "x = "
-              << std::endl << x
-              << std::endl;
+    std::cout << "x = " << x.transpose() << std::endl << std::endl;
 
     LTFIR_lowrank H(h,k);
     VectorXd y_H = H(x);
-    std::cout << "Vector y is:"
-              << std::endl
-              << y_H
-              << std::endl;
-//    Vector y should be:
-//    0.980805
-//     2.99719
-//     5.99522
-//     9.97489
-//     14.9901
-//     20.9862
-//     20.1166
-//     17.8828
-//      14.991
-//     11.1175
-//     5.88483
+    VectorXd y_exact(2*n-1);
+    y_exact <<
+            0.980805,
+            2.99719,
+            5.99522,
+            9.97489,
+            14.9901,
+            20.9862,
+            20.1166,
+            17.8828,
+            14.991,
+            11.1175,
+            5.88483;
+
+    std::cout << "Vector y is:" << std::endl << y_H << std::endl << std::endl
+              << "Error: " << (y_H - y_exact).norm() << std::endl << std::endl;
 
     // PART 3: filter vector x (FFT-based filter)
     std::cout << "**** PART 3 ****" << std::endl;
@@ -178,4 +172,7 @@ int main() {
     LTFIR_freq F(h,k);
     VectorXd y_F = F(x);
     std::cout << "y_F:" << std::endl << y_F << std::endl;
+
+    return 0;
 }
+
