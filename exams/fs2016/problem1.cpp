51eabb2d7eb8e7a7f128b3695c9b557c2b69d23f
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-20 10:14:36 +0100

    Implement symmetric Gauss-Seidel iteration

--- exams/fs2016/problem1.cpp
+++ exams/fs2016/problem1.cpp
@@ -10,34 +10,46 @@ using Matrix = Eigen::MatrixXd;
 //! \param[in,out] x initial guess as input and last value of iteration as output
 //! \param[in] rtol relative tolerance for termination criteria
 void GSIt(const Matrix & A, const Vector & b, Vector & x, double rtol) {
+    Eigen::TriangularView<Matrix, Eigen::Upper> UpD(A);
+    Eigen::TriangularView<Matrix, Eigen::Lower> LpD(A);
+    Eigen::TriangularView<Matrix, Eigen::StrictlyUpper> U(A);
+    Eigen::TriangularView<Matrix, Eigen::StrictlyLower> L(A);
 
-    // TODO: problem 1c: implement a Gauss-Seidel solver for Ax = b
+    Vector x_new = x;
 
-    return;
+    while (true) {
+        x_new = UpD.solve(b) - UpD.solve(L*LpD.solve(b - U*x));
+
+        double r = (x_new - x).norm();
+
+        if (r < rtol * x_new.norm()) {
+            return;
+        }
+
+        x = x_new;
+    }
 }
 
 
 int main(int, char**) {
-    // Build matrix and r.h.s.
-    unsigned int n = 9; // A is n x n, b and x have length n
+    const unsigned int n = 9; // A is n x n, b and x have length n
 
     Matrix A = Matrix::Zero(n,n);
-    for(unsigned int i = 0; i < n; ++i) {
-        if(i > 0) A(i,i-1) = 2;
+    for (unsigned int i = 0; i < n; ++i) {
+        if (i > 0) A(i,i-1) = 2;
         A(i,i) = 3;
-        if(i < n-1) A(i,i+1) = 1;
+        if (i < n-1) A(i,i+1) = 1;
     }
     Vector b = Vector::Constant(n,1);
 
-    //// PROBLEM 1d
     std::cout << "*** PROBLEM 1d:" << std::endl;
+    Vector x = Vector::Random(n);
+    GSIt(A, b, x, 10e-8);
 
-    // TODO: problem 1d: test the code GSIt using the given data
-
-    double residual = 0.; // TODO: problem 1d: compute residual
+    double residual = (A*x - b).norm();
 
     std::cout << "Residual = " << residual << std::endl;
 
-	return 0;
+    return 0;
 }
 
