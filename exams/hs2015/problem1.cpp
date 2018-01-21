6d7f7f4621271ea3229ad763bb2195e7da87dae6
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-22 00:30:37 +0100

    Implement stable computation of complex roots

--- exams/hs2015/problem1.cpp
+++ exams/hs2015/problem1.cpp
@@ -7,11 +7,19 @@
 //! \param[in] w complex number with non negative imaginary part
 //! \return the square root of w with non negative real and imaginary parts
 std::complex<double> myroot( std::complex<double> w ) {
-    double x,y;
+    double x, y;
     double u = w.real();
     double v = w.imag();
 
-    // TODO: problem 1c: construct x and y as functions of u and v
+    if (v == 0) return std::sqrt(u);
+
+    if (u > 0) {
+        x = std::sqrt((std::sqrt(u*u + v*v) + u) / 2.);
+        y = v / (2.*x);
+    } else {
+        y = std::sqrt((std::sqrt(u*u + v*v) - u) / 2.);
+        x = v / (2.*y);
+    }
 
     return std::complex<double> (x,y);
 }
@@ -22,12 +30,14 @@ int main() {
 
     std::complex<double> w(1e20,5);
     std::cout << "The square root of " << w << " is " << myroot(w) << std::endl;
-    std::cout << "The correct square root of " << w << " is " << sqrt(w) << std::endl << std::endl;
+    std::cout << "The correct square root of " << w << " is " << sqrt(w)
+              << std::endl << std::endl;
 
     w=std::complex<double>(-5,1e20);
     std::cout << "The square root of " << w << " is " << myroot(w) << std::endl;
-    std::cout << "The correct square root of " << w << " is " << sqrt(w) << std::endl << std::endl;
+    std::cout << "The correct square root of " << w << " is " << sqrt(w)
+              << std::endl << std::endl;
 
-	return 0;
+    return 0;
 }
 
