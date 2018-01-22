f7fa9c2b883311ab52fb4fe0c0264452509d78d5
Anian Ruoss <anian.ruoss@gmail.com>
2018-01-22 01:08:53 +0100

    Implement ELLPACK sparse matrix format

--- exams/hs2015/problem2.cpp
+++ exams/hs2015/problem2.cpp
@@ -12,7 +12,6 @@ using index_t = std::ptrdiff_t;
 //! \brief Class representing a sparse matrix in Ellpack format
 class EllpackMat {
   public:
-
     EllpackMat(const Triplets & triplets, index_t m, index_t n);
 
     double operator() (index_t i, index_t j) const;
@@ -33,10 +32,40 @@ class EllpackMat {
 //! \param[in] m rows of matrix
 //! \param[in] n columns of matrix
 //! \param[in] maxcols max. number of nonzeros of the matrix
-EllpackMat::EllpackMat(const Triplets & triplets, index_t m, index_t n)
-    : m(m), n(n) {
-    // TODO: problem 2a: implement constructor building matrix from Eigen Triplet format
+EllpackMat::EllpackMat(const Triplets & triplets,
+                       index_t m, index_t n) : m(m), n(n) {
+    std::vector<unsigned int> counters(m);
+    std::fill(counters.begin(), counters.end(), 0);
 
+    maxcols = 0;
+
+    for	(const auto tr : triplets) {
+        if (++counters[tr.row()] > maxcols) {
+            maxcols = counters[tr.row()];
+        }
+    }
+
+    col.resize(m*maxcols, -1);
+    val.resize(m*maxcols, 0);
+
+    for (const auto tr : triplets) {
+        assert(0 <= tr.row() && tr.row() < m && 0 <= tr.col() && tr.col() < n &&
+               "Index out of bounds!");
+
+        index_t I;
+        for (I = tr.row()*maxcols; I < (tr.row()+1)*maxcols; ++I) {
+            if (col[I] == tr.col()) {
+                val[I] += tr.value();
+                break;
+            } else if (col[I] == -1) {
+                col[I] = tr.col();
+                val[I] = tr.value();
+                break;
+            }
+        }
+
+        assert(I < (tr.row()+1)*maxcols && "Not enough colums reserved!");
+    }
 }
 
 //! \brief Retrieve value of entry at index (i,j)
@@ -60,8 +89,14 @@ void EllpackMat::mvmult(const Vector &x, Vector &y) const {
     assert(x.size() == n && "Incompatible vector x size!");
     assert(y.size() == m && "Incompatible vector y size!");
 
-    // TODO: problem 2b: implement operation y = this*x
+    y = Vector::Zero(m);
 
+    for (index_t i = 0; i < m; ++i) {
+        for (index_t l = i*maxcols; l < (i+1)*maxcols; ++l) {
+            if (col[l] == -1) break;
+            y(i) += x(col[l]) * val[l];
+        }
+    }
 }
 
 //! \brief Computes (*this)'*x, A' is transposed
@@ -71,9 +106,16 @@ void EllpackMat::mtvmult(const Vector &x, Vector &y) const {
     assert(x.size() == m && "Incompatible vector x size!");
     assert(y.size() == n && "Incompatible vector y size!");
 
-    // TODO: problem 2c: implement operation y = (this^T)*x
+    y = Vector::Zero(n);
 
+    for (index_t i = 0; i < m; ++i) {
+        for (index_t l = i*maxcols; l < (i+1)*maxcols; ++l) {
+            if (col[l] == -1) break;
+            y(col[l]) += x(i) * val[l];
+        }
+    }
 }
+
 
 int main(int, char**) {
     // Vector of triplets
@@ -111,16 +153,17 @@ int main(int, char**) {
     x << 4,5,6,7,8,9;
     Eigen::VectorXd Ex = Eigen::VectorXd::Zero(m);
     E.mvmult(x, Ex);
-    std::cout << "Sparse S*x =" << std::endl << S*x << std::endl;
-    std::cout << "Ellpack E*x =" << std::endl << Ex << std::endl;
+    std::cout << "Sparse S*x  = " << (S*x).transpose() << std::endl;
+    std::cout << "Ellpack E*x = " << Ex.transpose() << std::endl << std::endl;
     std::cout << " ------------- Test of x = A*y ------------- " << std::endl;
     Eigen::VectorXd y(3);
     y << 1,2,3;
     Eigen::VectorXd Ey = Eigen::VectorXd::Zero(n);
     E.mtvmult(y, Ey);
-    std::cout << "Sparse S^T*y =" << std::endl << S.transpose()*y  << std::endl;
-    std::cout << "Ellpack E^T*y =" << std::endl << Ey << std::endl;
+    std::cout << "Sparse S^T*y  = " << (S.transpose()*y).transpose()
+              << std::endl;
+    std::cout << "Ellpack E^T*y = " << Ey.transpose() << std::endl;
 
-	return 0;
+    return 0;
 }
 
